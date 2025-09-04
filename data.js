b666a3d041652a3953cd272448d0415320c488fe
Alex Millatmal <alexandrajmh@gmail.com>
2025-09-04 12:37:44 -0400

    chore: removes data

--- data.js
+++
@@ -1,4 +0,0 @@
-export const fullClass = ["Amethyst","Garnet","Pearl","Steven","Connie","Greg",
-        "Peridot","Lapis","Jasper", "Lion", "Onion"];
-
-export const disapprovedPairs = [["Onion", "Lapis"],["Jasper", "Peridot"]];
