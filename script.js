0ac29b6f11e73c3459935828eccc5986880312f5
Alex Millatmal <alexandrajmh@gmail.com>
2025-09-15 14:57:36 -0400

    refactor: moves functionality to utils to be tested

--- script.js
+++ script.js
@@ -3,12 +3,7 @@ import { fullClass as FULL_CLASS, disapprovedPairs as DISAPPROVED_PAIRS } from "
 // expects associated data of shapes:
 // fullClass: [String]
 // disapprovedPairs: [[String]], where inner arrays are expected to be of length 2. 
-
-// example data below can be uncommented (along with commenting _out_ the import statement above) to run the script directly:
-// const FULL_CLASS = ["Amethyst","Garnet","Pearl","Steven","Connie","Greg",
-//         "Peridot","Lapis","Jasper", "Lion", "Onion"];
-
-// const DISAPPROVED_PAIRS = [["Onion", "Lapis"],["Jasper", "Peridot"]];
+import { buildUnapprovedMap, fisherYatesShuffle, recursiveCheckAndSplit } from "./utils.js";
 
 // grabs arguments k,v pairs from user command-line input
 function getArgs() {
@@ -23,79 +18,7 @@ function getArgs() {
   return params;
 }
 
-// performant shuffle
-function fisherYatesShuffle(arr) {
-    for (let i= arr.length -1; i >= 1; i--) {
-        const j = Math.floor(Math.random() * (i+1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-}
-
 const ARGS = getArgs();
-let UNAPPROVED_MAP = {};
-
-// maps "bad matches" by user name, for quick look up
-function buildUnapprovedMap() {
-    DISAPPROVED_PAIRS.forEach((pair) => {
-        pair.forEach((student, i) => {
-            const badMatch = i > 0 ? pair[0] : pair[1] // hard-coded for pairs
-            if (UNAPPROVED_MAP[student]) {
-                const arrCopy = UNAPPROVED_MAP[student]
-                arrCopy.push(badMatch) 
-                UNAPPROVED_MAP[student] = arrCopy
-            } else {
-                UNAPPROVED_MAP[student] = [badMatch]
-            }
-        })
-    })
-}
-
-// splits up the class and creates groups,
-// if a bad match occurs, calls itself again
-function recursiveCheckAndSplit(fullClass, num, threshold) {
-    let groups = [];
-    for (let i= 0; i < fullClass.length; i += num) {
-        const chunk = fullClass.slice(i, i+num)
-        const badMatch = checkGroup(chunk)
-        if(badMatch !== null){
-            // find its index in the larger class arr
-            const targetIndex = fullClass.indexOf(badMatch)
-            // splice out the offender and...
-            const removedStudentArr = fullClass.splice(targetIndex, 1) // removes item and IMPORTANTLY shortens arr by 1
-            // in the case we are at the end...
-            if (i >= (num*(threshold-1))) {
-                // move them to the front of the shuffle
-                fullClass.splice(0, 0, removedStudentArr[0])
-            // otherwise,
-            } else {
-                // move the offender to just after this chunk
-                fullClass.splice(i+num+2, 0, removedStudentArr[0]) // so we offset index by 2 here
-            }
-            // and start over
-            groups = recursiveCheckAndSplit(fullClass, num, threshold) 
-        } else {
-            groups.push(chunk)
-        }
-    }
-    return groups
-}
-
-// determines if a group containes a bad match
-function checkGroup(group) {
-    let firstBadMatch = null;
-
-        for (const [k, v] of Object.entries(UNAPPROVED_MAP)) {
-            if (group.includes(k)) {
-                v.forEach((student, i) => {
-                    if (group.includes(student)) {
-                        firstBadMatch = student
-                    }
-                })
-            }
-        }
-    return firstBadMatch
-}
 
 // main caller / script driver
 if (ARGS.number) {
@@ -104,9 +27,9 @@ if (ARGS.number) {
         console.warn("Please provide a number less than the full class size of", FULL_CLASS.length.toString());
     } else {
         const threshold = Math.ceil(FULL_CLASS.length/COUNT) // total number of groups we will have
-        buildUnapprovedMap();
+        const UNAPPROVED_MAP = buildUnapprovedMap(DISAPPROVED_PAIRS);
         const shuffled = fisherYatesShuffle(FULL_CLASS);
-        const groups = recursiveCheckAndSplit(shuffled, COUNT, threshold);
+        const groups = recursiveCheckAndSplit(shuffled, COUNT, threshold, UNAPPROVED_MAP);
         console.log("groups: ", groups.slice(0, threshold)) // TODO: figure out why the recursion is working such that this slice is necessary.
     }
 } else {
---
+++ utils.js
@@ -0,0 +1,73 @@
+// maps "bad matches" by user name, for quick look up
+export function buildUnapprovedMap(DISAPPROVED_PAIRS) {
+    let map = {}
+    DISAPPROVED_PAIRS.forEach((pair) => {
+        pair.forEach((student, i) => {
+            const badMatch = i > 0 ? pair[0] : pair[1] // hard-coded for pairs
+            if (map[student]) {
+                const arrCopy = map[student]
+                arrCopy.push(badMatch) 
+                map[student] = arrCopy
+            } else {
+                map[student] = [badMatch]
+            }
+        })
+    })
+    return map;
+}
+
+// determines if a group containes a bad match
+export function checkGroup(group, UNAPPROVED_MAP) {
+    let firstBadMatch = null;
+
+        for (const [k, v] of Object.entries(UNAPPROVED_MAP)) {
+            if (group.includes(k)) {
+                v.forEach((student, i) => {
+                    if (group.includes(student)) {
+                        firstBadMatch = student
+                    }
+                })
+            }
+        }
+    return firstBadMatch
+}
+
+// performant shuffle
+export function fisherYatesShuffle(arr) {
+    for (let i= arr.length -1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * (i+1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
+// splits up the class and creates groups,
+// if a bad match occurs, calls itself again
+export function recursiveCheckAndSplit(fullClass, num, threshold, UNAPPROVED_MAP) {
+    let groups = [];
+    console.log({UNAPPROVED_MAP})
+    for (let i= 0; i < fullClass.length; i += num) {
+        const chunk = fullClass.slice(i, i+num)
+        const badMatch = checkGroup(chunk, UNAPPROVED_MAP)
+        if(badMatch !== null){
+            // find its index in the larger class arr
+            const targetIndex = fullClass.indexOf(badMatch)
+            // splice out the offender and...
+            const removedStudentArr = fullClass.splice(targetIndex, 1) // removes item and IMPORTANTLY shortens arr by 1
+            // in the case we are at the end...
+            if (i >= (num*(threshold-1))) {
+                // move them to the front of the shuffle
+                fullClass.splice(0, 0, removedStudentArr[0])
+            // otherwise,
+            } else {
+                // move the offender to just after this chunk
+                fullClass.splice(i+num+2, 0, removedStudentArr[0]) // so we offset index by 2 here
+            }
+            // and start over
+            groups = recursiveCheckAndSplit(fullClass, num, threshold, UNAPPROVED_MAP) 
+        } else {
+            groups.push(chunk)
+        }
+    }
+    return groups
+}
